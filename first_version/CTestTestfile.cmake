dcd87dcd9b1088ab37d05ae9df28dbce0b42fdd4
Jawad0695 <khanjawadahmed74@gmail.com>
2024-08-29 20:01:27 +0500

    Add files via upload

---
+++ first_version/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/smaunrizvi/fyp_ws/src/first_version
+# Build directory: /home/smaunrizvi/fyp_ws/build/first_version
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
