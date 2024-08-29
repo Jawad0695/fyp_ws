734f322161a225c8d9bd06a704083e1f12643043
Jawad0695 <khanjawadahmed74@gmail.com>
2024-08-29 19:59:29 +0500

    Add files via upload

---
+++ catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/smaunrizvi/fyp_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/smaunrizvi/fyp_ws/devel;/opt/ros/noetic'.split(';') if '/home/smaunrizvi/fyp_ws/devel;/opt/ros/noetic' != '' else []
